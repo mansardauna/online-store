f7f4c8a4816cc051ae079f29f23fdf4a97649b18
mansardauna <olaomansur@gmail.com>
2023-09-12 11:51:26 -0400

    profilePage

--- src/pages/About/About.js
+++ src/pages/About/About.js
@@ -1,4 +1,8 @@
 import React, { useEffect, useState } from "react";
+import { BiHistory, BiMoney, BiUser } from "react-icons/bi";
+import { BsFillBagCheckFill } from "react-icons/bs";
+import { FaAddressBook } from "react-icons/fa";
+import { MdSettings } from "react-icons/md";
 import { Link, useLocation } from "react-router-dom";
 import Breadcrumbs from "../../components/pageProps/Breadcrumbs";
 
@@ -6,32 +10,32 @@ const aboutItems = [
   {
     title: "Profile",
     link: "/profile",
-    icon: "",
+    icon: <BiUser/>
   },
   {
     title: "Order",
     link: "/order",
-    icon: "",
+    icon: <BsFillBagCheckFill/>,
   },
   {
     title: "Payment Method",
     link: "/payment",
-    icon: "", 
+    icon: <BiMoney/> 
   },
   {
     title: "Address",
     link: "/address",
-    icon: "", 
+    icon: <FaAddressBook/> 
   },
   {
     title: "Settings",
     link: "/settings",
-    icon: "",
+    icon: <MdSettings/>
   },
   {
     title: "History",
     link: "/history",
-    icon: "", 
+    icon: <BiHistory/> 
   },
 ];
 
@@ -46,12 +50,12 @@ const About = () => {
     <div className="max-w-container mx-auto px-4">
       <Breadcrumbs title="Settings" prevLocation={prevLocation} />
       <div>
-        <div className="grid grid-cols-2 w-10/12 m-auto p-2 gap-4 md:grid-cols-3">
+        <div className="grid grid-cols-2 md:w-9/12 w-10/12 m-auto p-2 gap-4 md:grid-cols-3">
           {aboutItems.map((item) => (
               <Link to={item.link}>
-            <div key={item.title} className=" w-11/12 bg-slate-300 hover:bg-slate-400 shadow-md rounded-md h-32 p-4 items-center"> 
-                <div>{item.icon}</div>
-                <div className="m-auto w-fit text-lg text-center md:text-xl font-semibold">{item.title}</div>
+            <div key={item.title} className=" md:w-9/12 w-11/12 bg-slate-100 hover:bg-slate-300 shadow-md rounded-md h-32 p-4 items-center"> 
+                <div className="item-center w-fit m-auto">{item.icon}</div>
+                <div className="m-auto mt-3 w-fit text-lg text-center md:text-xl">{item.title}</div>
             </div>
               </Link>
           ))}
