b21b94ee44c5d078ec1851912a4b1acd14cdc856
mansardauna <olaomansur@gmail.com>
2023-09-28 09:07:01 -0400

    reciept

--- src/pages/Cart/ReceiptModal.js
+++ src/pages/Cart/ReceiptModal.js
@@ -4,7 +4,7 @@ import Modal from 'react-modal';
 const ReceiptModal = ({ isOpen, closeModal, children }) => {
   return (
     <Modal isOpen={isOpen} onClose={closeModal}
-    className="m-auto mt-40 md:h-1/2 h-[65%] w-10/12 md:w-4/12 p-4  bg-gray-100">
+    className="m-auto mt-40 md:h-1/2 h-[65%] w-10/12 md:w-4/12 border border-gray-400 rounded-md p-4  bg-gray-100">
       {children}
     </Modal>
   );
